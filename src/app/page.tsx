0fbcb18d41f543a2b7530170c792b596abd41413
AdrianMartinezGit <adrionmtz02@gmail.com>
2024-06-11 03:14:14 -0700

    Deploy Everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
     <>
       <Navbar fluid style={{position: 'fixed', top: 0, left: 0, width: '100%', backgroundColor: 'rgb(128, 192, 128)'}}>
         <Navbar.Brand>
-          <span className="self-center whitespace-nowrap text-xl font-semibold text-white" style={{ textShadow: '#000000 2px 2px 2px' }}>Adrian Martinez's Portfolio</span>
+          <span className="self-center whitespace-nowrap text-xl font-semibold text-white" style={{ textShadow: '#000000 2px 2px 2px' }}>Adrian Martinez</span>
         </Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse>
